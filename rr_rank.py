8b31e14798b5b0d6c8e534e81c07f45056e2abb5
Gaurav Chakravorty <gchak207@gmail.com>
2022-12-02 21:05:18 +0000

    pass asingle item test

--- rr_rank.py
+++ rr_rank.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 from typing import List
 from item import Item
+from random import shuffle
 
 
 @dataclass
@@ -12,6 +13,41 @@ class RRItem:
     rank_feature: float
 
 
+def rank_to_feature(rank: int) -> float:
+    """Converts the rank into a float value to be used in round robin ranking."""
+    return 1 / float(max(1, rank))
+
+
+def generate_offsets(numlists: int) -> List[float]:
+    """These offsets are used by the round robin ranker to maintain the same order of"""
+    offsets = [0.0] * numlists
+
+    for i in range(1, len(offsets)):
+        offsets[i] = offsets[i - 1] + 0.001
+        # 0.001 is small emnough to not interfere with rr ranking since result-lists should contain less than 100 items.
+
+    # random shuffling ensures that we are being fair to CGs and each CG is getting an
+    # equal chance of being at rank 1 even under round-robin ranking.
+    shuffle(offsets)
+    return offsets
+
+
 def rr_rank(results: List[List[Item]]) -> List[Item]:
     """round robin ranker"""
-    return []
+    rr_items: List[RRItem] = []
+    id_to_idx: dict = {}
+    offsets = generate_offsets(len(results))
+    for res_idx, ranklist in enumerate(results):
+        for item_idx, item in enumerate(ranklist):
+            if item.id not in id_to_idx:
+                # item has not been seen in any other candidate generator so far
+                idx = len(rr_items)
+                rr_item = RRItem(item, rank_to_feature(item_idx + 1) + offsets[res_idx])
+                rr_items.append(rr_item)
+                id_to_idx[item.id] = idx
+            else:
+                rr_items[id_to_idx[item.id]].rank_feature += (
+                    rank_to_feature(item_idx + 1) + offsets[res_idx]
+                )
+    rr_items.sort(key=lambda x: x.rank_feature, reverse=True)
+    return [x.item for x in rr_items]
