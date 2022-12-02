5dbc74e68a5245a52cb5415d0058346558233bc3
Gaurav Chakravorty <gchak207@gmail.com>
2022-12-02 21:09:53 +0000

    pass asingle item test

--- test_rr_rank.py
+++ test_rr_rank.py
@@ -1,5 +1,6 @@
 from rr_rank import rr_rank
 from item import Item
+import random
 
 
 def test_empty():
@@ -10,6 +11,21 @@ def test_empty():
 
 def test_single():
     """With single input, this should be an identity function"""
-    results = [[Item("abc", 1, None)]]
+    results = [[Item("a", 1, None)]]
     x = rr_rank(results=results)
-    assert x == [Item("abc", 1, None)]
+    assert x == [Item("a", 1, None)]
+
+
+def test_two_same_cg():
+    """With two inputs from same CG"""
+    results = [[Item("a", 1, None), Item("b", 0.9, None)]]
+    x = rr_rank(results=results)
+    assert x == [Item("a", 1, None), Item("b", 0.9, None)]
+
+
+def test_two_diff_cg():
+    """With two inputs from same CG"""
+    results = [[Item("a", 1, None)], [Item("b", 10, None)]]
+    random.seed(4)
+    x = rr_rank(results=results)
+    assert x == [Item("a", 1, None), Item("b", 10, None)]
